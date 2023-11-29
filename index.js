5a42308e6bcd2bf33ae2b7872c9975499962330c
insanelygreatsoftware <102649951+insanelygreatsoftware@users.noreply.github.com>
2023-11-28 17:41:14 -0800

    update port to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
